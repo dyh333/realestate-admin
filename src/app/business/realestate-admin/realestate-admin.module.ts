1fa4780b4527b2f9132e7fade76b7adab4908ed8
wangjx <774326739@qq.com>
2018-06-16 10:21:26 +0800

    update the header content of realestate-admin

--- src/app/business/realestate-admin/realestate-admin.module.ts
+++ src/app/business/realestate-admin/realestate-admin.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RealestateAdminComponent } from './realestate-admin.component';
 import { RealestateAdminRoutesModule } from './realestate-admin.routing';
-import { NzMenuModule } from 'ng-zorro-antd';
+import {
+  NzMenuModule,
+  NzDropDownModule
+} from 'ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
     NzMenuModule,
+    NzDropDownModule,
     RealestateAdminRoutesModule
   ],
   declarations: [RealestateAdminComponent]
