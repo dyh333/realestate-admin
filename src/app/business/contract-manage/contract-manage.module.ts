8e8309d4d6567f9885545fac745ceb8072c7ae82
dingyihui <dingyihui@cmss.chinamobile.com>
2018-06-19 20:08:10 +0800

    add modules
    Code Source From: Self Code
    
    Description:
    Jira:
    市场项目编号（名称）:

---
+++ src/app/business/contract-manage/contract-manage.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContractManageComponent } from './contract-manage.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ContractManageComponent]
+})
+export class ContractManageModule { }
---
+++ src/app/business/group-manage/group-manage.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GroupManageComponent } from './group-manage.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GroupManageComponent]
+})
+export class GroupManageModule { }
